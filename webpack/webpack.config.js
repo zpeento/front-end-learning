1d8829bedd430678df29ac7f17e1aafaac98fd14
iceiceice <509887158@qq.com>
2017-09-16 21:15:06 +0800

    add:    添加webpack-dev-server功能

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,5 +4,11 @@ module.exports = {
 	output:{
 		path: __dirname  + "/public",//打包后存放的地方
 		filename: 'bundle.js' // 打包后输出的文件名
-	}
+	},
+
+	devServer: {
+	    contentBase: "./public",//本地服务器所加载的页面所在的目录
+	    historyApiFallback: true,//不跳转
+	    inline: true//实时刷新
+	 }
 }
