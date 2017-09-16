1d4ada45dac42cd4cf43798ec3161d6bfe13686d
iceiceice <509887158@qq.com>
2017-09-16 21:31:29 +0800

    change:    添加babel配置

--- webpack/app/Greeter.js
+++ webpack/app/Greeter.js
@@ -1,5 +1,7 @@
+var config = require('./config.json')
+
 module.exports = function() {
-	var greet = document.createElement('div');
-	greet.textContent = 'hello world';
+	let greet = document.createElement('div');
+	greet.textContent = config.greetText;
 	return greet; 
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -10,5 +10,18 @@ module.exports = {
 	    contentBase: "./public",//本地服务器所加载的页面所在的目录
 	    historyApiFallback: true,//不跳转
 	    inline: true//实时刷新
-	 }
+	},
+
+	module: {
+		rules: [
+			{
+				test: /(\.jsx|\.js)$/,
+				use: {
+					loader: "babel-loader",
+					presets: ["es2015", "react"]
+				},
+				exclude: /node_modules/
+			}
+		]
+	}
 }
